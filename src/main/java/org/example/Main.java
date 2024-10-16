c9bc41041984671541fd964918022c572e59a964
neilhanda <neilhanda83@gmail.com>
2024-10-16 21:34:38 +0530

    Viva

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         System.out.println("------------Scientific Calculator--------------");
         do {
-//            this is comment7
+//            this is comment_new
             System.out.println("Choices of Operations:");
             System.out.println("1. Square Root");
             System.out.println("2. Factorial");
